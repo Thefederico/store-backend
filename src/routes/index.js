13445228fa7b3696695cfd459b553c5db9910300
Thefederico <federico_upegui.dev@outlook.com>
2022-07-26 14:26:32 -0500

    chore: Added role for user

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ const express = require('express')
 
 const productsRouter = require('./products.router')
 const usersRouter = require('./users.router')
+const custosmerRouter = require('./customers.router')
 
 function routesApi (app) {
   const router = express.Router()
   app.use('/api/v1', router)
   router.use('/products', productsRouter)
   router.use('/users', usersRouter)
+  router.use('/customers', custosmerRouter)
 }
 
 module.exports = routesApi
