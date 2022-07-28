058b301152d1a15024126429c35f6b3945aa3641
Federico Upegui Restrepo <federico_upegui.dev@outlook.com>
2022-07-27 20:58:18 -0500

    chore: Categories route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const productsRouter = require('./products.router')
 const usersRouter = require('./users.router')
 const custosmerRouter = require('./customers.router')
+const categoriesRouter = require('./categories.router')
 
 function routesApi (app) {
   const router = express.Router()
@@ -10,6 +11,7 @@ function routesApi (app) {
   router.use('/products', productsRouter)
   router.use('/users', usersRouter)
   router.use('/customers', custosmerRouter)
+  router.use('/categories', categoriesRouter)
 }
 
 module.exports = routesApi
