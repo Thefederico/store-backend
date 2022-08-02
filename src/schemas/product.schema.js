d141ff30220084f67671884d01f1a5325db6a6a4
Federico Upegui Restrepo <federico_upegui.dev@outlook.com>
2022-08-01 19:29:51 -0500

    Feature: Pagination & price filter

--- src/schemas/product.schema.js
+++ src/schemas/product.schema.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 const Joi = require('joi')
 
 const id = Joi.number().integer()
@@ -6,10 +7,15 @@ const name = Joi.string()
   .max(15)
 const price = Joi.number()
   .integer()
-  .min(10)
+  .min(3)
 const image = Joi.string().uri()
 const description = Joi.string().min(10)
 const categoryId = Joi.number().integer()
+
+const limit = Joi.number().integer()
+const offset = Joi.number().integer()
+const price_min = Joi.number().integer()
+const price_max = Joi.number().integer()
 
 const createProductSchema = Joi.object({
   name: name.required(),
@@ -31,4 +37,20 @@ const getProductSchema = Joi.object({
   id: id.required()
 })
 
-module.exports = { createProductSchema, updateProductSchema, getProductSchema }
+const queryProductSchema = Joi.object({
+  limit,
+  offset,
+  price,
+  price_min,
+  price_max: price_max.when('price_min', {
+    is: Joi.number().integer(),
+    then: Joi.required()
+  })
+})
+
+module.exports = {
+  createProductSchema,
+  updateProductSchema,
+  getProductSchema,
+  queryProductSchema
+}
